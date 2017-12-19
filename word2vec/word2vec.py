8e7d0a551b8a3d641d6fc83355533ed40d64d7af
kkushaev <kkushaev@mirantis.com>
2017-12-19 23:05:52 +0400

    algorithm

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -1,2 +1,12 @@
 import torch
 
+
+# cbow model
+# initialize data
+#   get size of vocab
+#   initialize word vectors
+#   initialize X and y
+# initialize neural network
+#    create neural network
+#    prepare optimizer
+# launch optimizer
